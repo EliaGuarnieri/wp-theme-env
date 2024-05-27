eb8dd115e189e0038d0a26456c124d647c3c60db
Elia Guarnieri <elia.guarnieri@gmail.com>
2024-05-27 20:39:11 +0200

    feat: Add global style

---
+++ functions.php
@@ -0,0 +1,12 @@
+<?php
+add_action('wp_enqueue_scripts', 'wordpress_theme_enqueue_styles');
+
+function wordpress_theme_enqueue_styles() {
+  wp_enqueue_style(
+    'wordpress-theme-style',
+    get_parent_theme_file_uri('assets/css/global.css'),
+    array(),
+    wp_get_theme()->get('Version'),
+    'all'
+  );
+}
