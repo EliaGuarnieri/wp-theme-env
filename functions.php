dd14c7b173721fed1aed561ab61fb6f4f01c1aab
Elia Guarnieri <elia.guarnieri@gmail.com>
2024-05-28 13:22:11 +0200

    chore: Add tailwindcss

--- functions.php
+++ functions.php
@@ -4,7 +4,7 @@ add_action('wp_enqueue_scripts', 'wordpress_theme_enqueue_styles');
 function wordpress_theme_enqueue_styles() {
   wp_enqueue_style(
     'wordpress-theme-style',
-    get_parent_theme_file_uri('assets/css/global.css'),
+    get_parent_theme_file_uri('assets/css/style.min.css'),
     array(),
     wp_get_theme()->get('Version'),
     'all'
